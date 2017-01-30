715dd9f6ae6f4f50965a47f3fe9f52feead2fc4e
Maggie Love <maggie.love1991@gmail.com>
2017-01-30 00:14:15 -0500

    Deletes js directory, which was renamed to src

--- js/almanac.js
+++
@@ -1,8 +0,0 @@
-import React, { PropTypes } from 'react';
-import classNames from 'classnames';
-
-export default function Almanac() {
-    return(
-        document.getElementById('root')
-    )
-}
